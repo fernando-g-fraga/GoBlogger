598a9283b9bdb7c7df510d4b0996fa17e465901f
fernando fraga <fernandogrfraga@gmail.com>
2024-09-13 11:16:57 -0300

    Lê e exibe os arquivos locais com base no slug da URL

---
+++ cat.md
@@ -0,0 +1,16 @@
+# Must-Have Items for Cat Owners
+
+## 1. Litter Box
+A clean and accessible litter box is essential for your cat's hygiene.
+
+## 2. Cat Food
+High-quality cat food that meets your cat's nutritional needs.
+
+## 3. Scratching Post
+A scratching post helps keep your cat's claws healthy and prevents damage to furniture.
+
+## 4. Toys
+Interactive toys to keep your cat entertained and active.
+
+## 5. Cat Bed
+A comfortable bed where your cat can rest and sleep.
--- main.go
+++ main.go
@@ -2,17 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
+
+type slugReader interface {
+	Read(slug string) (string, error)
+}
+
+type FileReader struct{}
+
+func (fsr FileReader) Read(slug string) (string, error) {
+	f, err := os.Open(slug + ".md")
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func PostHandler(sl slugReader) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slug := r.PathValue("slug")
+		markdown, err := sl.Read(slug)
+
+		if err != nil {
+			http.Error(w, "Post not found", http.StatusNotFound)
+			return
+		}
+		fmt.Fprint(w, markdown)
+	}
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/post/{slug}", func(w http.ResponseWriter, r *http.Request) {
-		slug := r.PathValue("slug")
-		fmt.Fprintf(w, "Post: %s", slug)
-	})
+	mux.HandleFunc("GET /post/{slug}", PostHandler(FileReader{}))
 
 	fmt.Println("Listening to port :3030")
 	err := http.ListenAndServe(":3030", mux)
